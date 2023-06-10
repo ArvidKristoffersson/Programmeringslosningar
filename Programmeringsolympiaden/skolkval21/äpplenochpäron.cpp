c361dfb24dc3ba557ebe31c16a94b92f114b4384
ArvidKristoffersson <117284329+ArvidKristoffersson@users.noreply.github.com>
2023-06-10 16:05:54 +0200

    Create äpplenochpäron.cpp

---
+++ Programmeringsolympiaden/skolkval21/äpplenochpäron.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b; cin >> a >> b; 
+
+    if (a*7 > b*13)cout << "Axel" << endl;
+    else if (a*7 < b*13) cout << "Petra" << endl;
+    else cout << "lika" << endl;
+
+    return 0;
+}
