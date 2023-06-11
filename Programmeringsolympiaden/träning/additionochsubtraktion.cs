252b3fa81e4c624d6b4d230f07ea54c6dca7c599
ArvidKristoffersson <117284329+ArvidKristoffersson@users.noreply.github.com>
2023-06-11 13:35:31 +0200

    Create additionochsubtraktion.cs

---
+++ Programmeringsolympiaden/träning/additionochsubtraktion.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+        long a = Int64.Parse(Console.ReadLine());
+        long b = Int64.Parse(Console.ReadLine());
+        Console.WriteLine(a+b);
+        Console.WriteLine(a-b);
+        Console.Read();
+        
+  }
+}
