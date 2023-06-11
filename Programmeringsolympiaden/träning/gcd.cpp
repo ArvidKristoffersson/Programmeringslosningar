476f4d44f939ed9227239f66481aa92bf0e066e4
ArvidKristoffersson <117284329+ArvidKristoffersson@users.noreply.github.com>
2023-06-11 13:39:44 +0200

    Create gcd.cpp

---
+++ Programmeringsolympiaden/träning/gcd.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <queue>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+
+#define all(x)  (x).begin(), (x).end()
+#define rall(x)  (x).rbegin(), (x).rend()
+
+typedef long long ll;
+
+ll linf = 1e15+1;
+
+using namespace std;
+
+inline void scoobydoobydoo(){
+    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+int gcd(int a, int b){
+    int ret = -1;
+
+    for (int i = 1; i <= min(a, b); i++){
+        if (a%i == 0 && b%i == 0){
+            ret = i;
+        }
+    }
+
+    return ret;
+}
