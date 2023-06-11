d9fa8c3fbd59acbe2fc15b0b0e16af2603eeae3f
ArvidKristoffersson <117284329+ArvidKristoffersson@users.noreply.github.com>
2023-06-11 13:45:43 +0200

    Create räknafunktioner.cpp

---
+++ Programmeringsolympiaden/träning/räknafunktioner.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int cube(int x){
+    return pow(x, 3);
+}
+
+int max(int x, int y){
+    if (x >= y){
+        return x;
+    } else {
+        return y;
+    };
+}
+
+int differens(int x, int y){
+    return x - y;
+}
