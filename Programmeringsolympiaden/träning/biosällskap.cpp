23fb266f4b6b2259d9605c64195676e353eccc4f
ArvidKristoffersson <117284329+ArvidKristoffersson@users.noreply.github.com>
2023-06-11 13:37:28 +0200

    Create biosällskap.cpp

---
+++ Programmeringsolympiaden/träning/biosällskap.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x, n; cin >> x >> n;
+    int arr[n], sum = 0, ans = 0;
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+        if (sum + arr[i] <= x){
+            sum += arr[i];
+        } else {
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
