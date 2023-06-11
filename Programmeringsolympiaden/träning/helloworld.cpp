6e4b72e12d602d15c7bdd5a8798df2d2b9e09d64
ArvidKristoffersson <117284329+ArvidKristoffersson@users.noreply.github.com>
2023-06-11 13:40:18 +0200

    Create helloworld.cpp

---
+++ Programmeringsolympiaden/träning/helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int input;
+
+  cin >> input;
+
+  for(int i = 0; i < input; i++){
+    cout << "Hello World!" << endl;
+  }
+
+  return 0;
+}
